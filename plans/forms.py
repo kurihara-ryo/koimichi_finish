32c5ced40df35abab9b0a018c6cc877b326dba7e
栗原菱 <ryoukurihara0@gmail.com>
2025-08-10 07:55:22 +0900

    交通手段追加

--- plans/forms.py
+++ plans/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from .models import Plan
+from .models import Plan, TRAVEL_MODES
 
 class PlanBasicForm(forms.ModelForm):
     class Meta:
         model = Plan
-        fields = ["title", "pref", "sub_area", "is_public"]
+        fields = ["title", "pref", "sub_area", "default_mode", "is_public"]
+        labels = {
+            "title": "タイトル",
+            "pref": "都道府県",
+            "sub_area": "エリア",
+            "default_mode": "交通手段",
+            "is_public": "公開設定",
+        }
+        widgets = {
+            "default_mode": forms.Select(choices=TRAVEL_MODES),
+        }
+        
