120f57921122b5dc6b5a8f16c65c505fd0d3cd93
cracker1013 <asakura.k_05@outlook.jp>
2025-08-10 12:09:50 +0900

    plans/viewsのボタン表示制御用からリセットボタンいじれる

--- dateapp/urls.py
+++ dateapp/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
 from plans import views as pv
+from plans.views_delete import plan_delete
+from plans.views_bulk_delete import bulk_delete
 
 
 urlpatterns = [
@@ -37,7 +40,10 @@ urlpatterns = [
     path("register/", pv.register, name="register"),
     
     path("plans/new/", pv.plan_new, name="plan_new"),  # 新規作成
+
     path("plans/<int:pk>/", pv.plan_detail, name="plan_detail"),  # 詳細表示
+    path("plans/<int:pk>/delete/", plan_delete, name="plan_delete"),  # 個別削除
+    path("plans/bulk_delete/", bulk_delete, name="bulk_delete"),  # 一括削除
 
     # 一覧＆検索
     path("plans/", pv.plan_list, name="plan_list"),
